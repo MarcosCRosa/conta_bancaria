71f9f0509aff9a229dc10e4ddd6c323c9e3fb72d
MarcosCRosa <marcosluiz4012@gmail.com>
2025-11-11 13:00:04 -0300

    feat:Criado interface para a conta e tambem os metodos abstract para o tratamento das contas

---
+++ src/repository/ContaRepository.ts
@@ -0,0 +1,10 @@
+import { Conta } from "../model/Conta";
+export interface ContaRepository{
+ procurarPorNumero(numero:number):void;
+ listarTodas():void;
+ cadastrar(conta:Conta):void;
+ atualizar(conta:Conta):void;
+ deletar(numero:number):void;
+
+ 
+}
